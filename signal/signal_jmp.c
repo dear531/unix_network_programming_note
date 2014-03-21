6f68461cee72b14b8a14d7a250024d47619ed2f9
zly <dear531@21cn.com>
2014-03-21 22:53:30 +0800

    test sigsetjmp and siglongjmp usage

--- signal/signal_jmp.c
+++ signal/signal_jmp.c
@@ -6,7 +6,14 @@
 
 char globle[] = "0";
 
+#define SAVE_MASK	0	/* flag of select either function setjmp and sigsetjmp */
+#if SAVE_MASK
+sigjmp_buf j;
+#else
 jmp_buf j;
+#endif
+
+/* signal handle function for SIGQUIT and SIGINT */
 void sig_quit(int signum)
 {
 	int i;
@@ -19,48 +26,52 @@ void sig_quit(int signum)
 		write(STDOUT_FILENO, &c, sizeof(c));
 		sleep(1);
 	}
-//	longjmp(j,1);
+#if SAVE_MASK
+	siglongjmp(j,1);
+#else
+	longjmp(j, 1);
+#endif
 	return;
 }
 
+/* main function */
 int main(int argc, char *argv[])
 {
 	struct sigaction act, actint, oact;
 	sigset_t set;
-#if 0
-           struct sigaction {
-               void     (*sa_handler)(int);
-               void     (*sa_sigaction)(int, siginfo_t *, void *);
-               sigset_t   sa_mask;
-               int        sa_flags;
-               void     (*sa_restorer)(void);
-           };
 
-#endif
-
+	/* set SIGQUIT function and mask etc */
 	act.sa_handler = sig_quit;
 	act.sa_flags = SA_NODEFER;
 	sigfillset(&act.sa_mask);
 	sigdelset(&act.sa_mask, SIGQUIT);
 	sigaction(SIGQUIT, &act, NULL);
 
+	/* set SIGINT function and mask etc */
 	actint.sa_handler = sig_quit;
 	actint.sa_flags = SA_NODEFER;
 	sigfillset(&actint.sa_mask);
 	sigdelset(&actint.sa_mask, SIGINT);
 	sigaction(SIGINT, &actint, NULL);
-
+#if 0
+	/* set system mask */
 	sigprocmask(SIG_SETMASK, NULL, &set);
 	sigdelset(&set, SIGQUIT);
 	sigdelset(&set, SIGINT);
 	sigprocmask(SIG_SETMASK, &set, NULL);
+#endif
 
-#if 0
+#if 1
+	/* setjmp and sigsetjmp loop check and test */
+# if SAVE_MASK
+	if (sigsetjmp(j, 1) == 0)
+# else
 	if (setjmp(j) == 0)
+# endif
 	{
 		for ( ; ; )
 		{
-			write(STDOUT_FILENO, );
+			write(STDOUT_FILENO, "m", sizeof("m"));
 			sleep(1);
 		}
 	}
@@ -68,11 +79,12 @@ int main(int argc, char *argv[])
 	{
 		for ( ; ; )
 		{
-			write();
+			write(STDOUT_FILENO, "r", sizeof("r"));
 			sleep(1);
 		}
 	}
 #endif
+	/* main loop show */
 	for ( ; ; )
 	{
 		write(STDOUT_FILENO, ".", sizeof("."));
