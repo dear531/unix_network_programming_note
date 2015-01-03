d3b6b06c7fa3c2381978e70d32821909df7c3d6a
root <dear531@21cn.com>
2015-01-03 13:02:09 +0800

    completed pipe

--- volume2/massege_pass/pipe_fifo/pipe_linearalgebra_incomplete.c
+++ volume2/massege_pass/pipe_fifo/pipe_linearalgebra_incomplete.c
@@ -1,6 +1,16 @@
+/*
+ * run:
+# ./a.out " 1 | 2 | 3 "
+pp[0]: 1 
+pp[1]: 2 
+pp[2]: 3 
+i:1, buff:1234
+
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
@@ -23,29 +33,46 @@ int main(int argc, char *argv[])
 	int j;
 	int pfd[10][2];
 	pid_t pid;
+	char buff[64] = {0};
 	for (i = 0; n > i; i++) {
 		int pfd[10][2];
 		pipe(pfd[i]);
 		pid = fork();
 		if (0 > pid) {
 			/* fork error */
-		} else if (0 < pid) {
-			/* parent */
+		} else if (0 == pid) {
+			/* child */
 			if (0 < i) {
 				close(pfd[i - 1][1]);
 			}
 			close(pfd[i][0]);
-			dup2(stdout, pfd[i][1]);
+			if (1 < i) {
+				close(pfd[i -2][0]);
+				close(pfd[i - 1][1]);
+			}
+			if(n - 1 == i) {
+			/* end */
+				read(pfd[i - 1][0], buff, sizeof(buff));
+				fprintf(stdout, "i:%d, buff:%s\n", i, buff);
+			} else {
+			/* mide */
+				read(pfd[i -1][0], buff, sizeof(buff));
+				write(pfd[i - 1][1], buff, strlen(buff));
+			}
 			
 			close(pfd[i][1]);
+			if (0 < i)
+				close(pfd[i -1][0]);
 			exit(EXIT_SUCCESS);
 		} else {
-			/* child */
-			if (1 < i)
-				close(pfd[i -1][0]);
-			close(pfd[i][1]);
+			/* parent */
+			if (0 == i) {
+			/* begin */
+				write(pfd[i][1], "1234", sizeof("1234"));
+			}
 		}
 	}
+	sleep(2);
 	
 	return 0;
 }
