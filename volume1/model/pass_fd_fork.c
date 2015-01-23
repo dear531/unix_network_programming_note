1e4c649b5f2f9b826cf26358dac356ae2e16f02e
zhangly <dear531@21cn.com>
2015-01-23 15:29:25 +0800

    compeletion pass file descriptor to child via fork server

--- volume1/model/pass_fd_fork.c
+++ volume1/model/pass_fd_fork.c
@@ -6,15 +6,17 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
 
 
 int sendfd(int pass, int source)
 {
-#define CONTEXT_RECV	""
+#define CONTEXT_NOTITY	""
 #define SOURCE_FILE		"2.txt"
 	struct iovec iov[1];
-	iov[0].iov_base = CONTEXT_RECV;
-	iov[0].iov_len = sizeof(CONTEXT_RECV);
+	iov[0].iov_base = CONTEXT_NOTITY;
+	iov[0].iov_len = sizeof(CONTEXT_NOTITY);
 	union control_un {
 		struct cmsghdr cmsg;
 		char msg_control[CMSG_SPACE(sizeof(int))];
@@ -100,79 +102,245 @@ failed:
 	fprintf(stdout, "normal error\n");
 	return -1;
 }
-
-int main(int argc, char *argv[])
+#define CONNECT_NUM	100
+int create_listen(void)
 {
-	/* fork */
-	pid_t pid;
-	/* create socketpair */
-	int pass[2];
-	int ret;
-	ret = socketpair(AF_UNIX, SOCK_STREAM, 0, pass);
-	if (0 > ret) {
-		fprintf(stderr, "socketpair error :%s\n",
+	int sockfd;
+	/* socket */
+#if 0
+	int socket(int domain, int type, int protocol);
+#endif
+	sockfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (0 > sockfd) {
+		fprintf(stderr, "socket error :%s\n",
 				strerror(errno));
 		exit(EXIT_FAILURE);
 	}
-	if (0 > (pid = fork())) {
-		/* fork error */
-	} else if (0 < pid) {
-		int source;
-		/* parent */
-		/* close socketpair 0 */
-		if (0 < pass[0]) {
-			close(pass[0]);
-			pass[0] = -1;
-		}
-		/* open file descriptor */
-		source = open(SOURCE_FILE, O_CREAT|O_WRONLY, S_IRUSR | S_IWUSR);
-		if (0 > source) {
-			fprintf(stderr, "open file %s error %s\n",
-					SOURCE_FILE, strerror(errno));
-			exit(EXIT_FAILURE);
-		}
-		/* send to child fd */
-		sendfd(pass[1], source);
-		/* close fd */
-		if (0 < source) {
-			close(source);
-			source = -1;
-		}
-		/* close socketpair 1 */
-		if (0 < pass[1]) {
-			close(pass[1]);
-			pass[1] = -1;
-		}
-	} else {
-		/* child */
-		int dest;
-		/* close socketpair 1 */
-		if (0 < pass[1]) {
-			close(pass[1]);
-			pass[1] = -1;
-		}
-		/* recv file descriptor from parent*/
-		recvfd(pass[0], &dest);
-		fprintf(stdout, "dest :%d\n", dest);
-		/* write fd */
-#define COUTEXT_WRITE	"12345"
-		int n;
-		if (0 > (n = write(dest, COUTEXT_WRITE, sizeof(COUTEXT_WRITE)))) {
-			fprintf(stderr, "write error in child :%s\n",
+	/* set addr ip reuse */
+	int opt = 1;
+	int ret;
+	ret = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+	if (0 > ret) {
+		fprintf(stderr, "setsockopt error :%s\n",
+				strerror(errno));
+	}
+	/* bind */
+#if 0
+	int bind(int sockfd, const struct sockaddr *my_addr, socklen_t addrlen);
+         struct sockaddr_in {
+             sa_family_t    sin_family; /* address family: AF_INET */
+             u_int16_t      sin_port;   /* port in network byte order */
+             struct in_addr sin_addr;   /* internet address */
+         };
+#endif
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;	
+	addr.sin_port = htons(8000);
+#if 0
+	int inet_pton(int af, const char *src, void *dst);
+#endif
+	inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
+	ret = bind(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+	if (0 > ret) {
+		fprintf(stderr, "bind error :%s\n",
+				strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	/* sockfd */
+	ret = listen(sockfd, CONNECT_NUM);
+	if (0 > ret) {
+		fprintf(stderr, "sockfd error :%s\n",
+				strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	return sockfd;
+}
+int main(int argc, char *argv[])
+{
+	/* create listen */
+	int listen;
+	listen = create_listen();
+	/* fork */
+	pid_t pid;
+	/* create socketpair */
+	struct {
+		int pass[2];
+		int status;
+	} cptr[CONNECT_NUM];
+	int ret;
+	int i;
+	for (i = 0; CONNECT_NUM	> i; i++) {
+		ret = socketpair(AF_UNIX, SOCK_STREAM, 0, cptr[i].pass);
+		if (0 > ret) {
+			fprintf(stderr, "socketpair error :%s\n",
 					strerror(errno));
 			exit(EXIT_FAILURE);
+		}
+		cptr[i].status = 0;
+		if (0 > (pid = fork())) {
+			/* fork error */
+		} else if (0 < pid) {
+			/* parent */
+			/* close socketpair 0 */
+			if (0 < cptr[i].pass[0]) {
+				close(cptr[i].pass[0]);
+				cptr[i].pass[0] = -1;
+			}
+#if 0
+			/* open file descriptor */
+			source = open(SOURCE_FILE, O_CREAT|O_WRONLY, S_IRUSR | S_IWUSR);
+			if (0 > source) {
+				fprintf(stderr, "open file %s error %s\n",
+						SOURCE_FILE, strerror(errno));
+				exit(EXIT_FAILURE);
+			}
+#else
+#endif
 		} else {
-			fprintf(stdout, "write success\n");
+			/* child */
+			/* close listen */
+			if (0 < listen) {
+				close(listen);
+				listen = -1;
+			}
+			int dest;
+			/* close socketpair 1 */
+			int j;
+			for (j = 0; i >= j; j++) {
+				if (0 < cptr[j].pass[1]) {
+#if 0
+					sleep(1);
+					fprintf(stdout, "getpid :%d, i:%d, close pass:%d\n",
+							getpid(), i, cptr[j].pass[1]);
+#endif
+					close(cptr[j].pass[1]);
+					cptr[j].pass[1] = -1;
+				}
+
+				/* other flower case is 0 is close in parent */
+			}
+			for ( ; ; ) {
+					/* recv file descriptor from parent*/
+					recvfd(cptr[i].pass[0], &dest);
+					fprintf(stdout, "dest :%d\n", dest);
+					/* write fd */
+					int n;
+					char buff[1024];
+					for ( ; memset(buff, 0x00, sizeof(buff)); ) {
+						n = recv(dest, buff, sizeof(buff), 0);
+						if (0 > n) {
+							/* recv error */
+							fprintf(stderr, "recv error :%s\n",
+									strerror(errno));
+						} else if (0 == n) {
+							/* peer close */
+							fprintf(stdout, "pid :%d peer close\n",
+									getpid());
+							break;
+						} else {
+							fprintf(stdout, "process id :%d buff :%s\n",
+									getpid(), buff);
+						}
+						send(dest, buff, strlen(buff), 0);
+					}
+					/* tell parent ready we're again */
+					send(cptr[i].pass[0], CONTEXT_NOTITY, sizeof(CONTEXT_NOTITY), 0);
+					/* close fd */
+					if (0 < dest) {
+						close(dest);
+						dest = -1;
+					}
+			}
+			/* close socketpair 0 */
+			if (0 < cptr[i].pass[0]) {
+				close(cptr[i].pass[0]);
+				cptr[i].pass[0] = -1;
+			}
+			exit(EXIT_SUCCESS);
 		}
-		/* close fd */
-		if (0 < dest) {
-			close(dest);
-			dest = -1;
+	}
+	/* send to child fd */
+	int source;
+	fd_set rset;
+	int n;
+	int fdmax = listen + 1, fdnum = 1;
+	int recvn;
+	char buff[512];
+	for ( ; ; ) {
+		/* select */
+		FD_ZERO(&rset);
+		FD_SET(listen, &rset);
+		for (i = 0; CONNECT_NUM > i; i++) {
+			if (1 == cptr[i].status) {
+				FD_SET(cptr[i].pass[1], &rset);
+			}
 		}
-		/* close socketpair 0 */
-		if (0 < pass[0]) {
-			close(pass[0]);
-			pass[0] = -1;
+#if 0
+       int select(int nfds, fd_set *readfds, fd_set *writefds,
+                  fd_set *exceptfds, struct timeval *timeout);
+#endif
+		n = select(fdmax, &rset, NULL, NULL, NULL);
+		fprintf(stdout, "n :%d\n", n);
+		if (0 > n) {
+			fprintf(stdout, "select error :%s\n",
+					strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+		if (FD_ISSET(listen, &rset)) {
+			/* accept */
+			source  = accept(listen, NULL, NULL);
+			if (0 > source) {
+				fprintf(stderr, "accept error :%s\n",
+						strerror(errno));
+			}
+			for (i = 0; CONNECT_NUM > i; i++) {
+				if (0 == cptr[i].status) {
+					sendfd(cptr[i].pass[1], source);
+					cptr[i].status = 1;
+					fdnum++;
+					if (cptr[i].pass[1] + 1 > fdmax) {
+						fdmax = cptr[i].pass[1] + 1;
+					}
+					break;
+				}
+			}
+			/* close fd */
+			if (0 < source) {
+				close(source);
+				source = -1;
+			}
+			n--;
+		}
+		for (i = 0; CONNECT_NUM > i; i++) {
+			if (1 == cptr[i].status && FD_ISSET(cptr[i].pass[1], &rset)) {
+				recv(cptr[i].pass[1], buff, sizeof(buff), 0);
+				cptr[i].status = 0;
+				fdnum--;
+				if (cptr[i].pass[1] + 1 == fdmax) {
+					int j, flag = 0;
+					for (j = i; 0 <= j; j--) {
+						if (1 == cptr[j].status) {
+							fdmax = cptr[j].pass[1] + 1;
+							flag = 1;
+							break;
+						}
+					}
+					if (0 == flag)
+						fdmax = listen + 1;
+				}
+				n--;
+				if (0 == n)
+					break;
+			}
+		}
+		fprintf(stdout, "fdmax:%d, fdnum:%d\n",
+				fdmax, fdnum);
+	}
+	for (i = 0; CONNECT_NUM > i; i++) {
+		/* close socketpair 1 */
+		if (0 < cptr[i].pass[1]) {
+			close(cptr[i].pass[1]);
+			cptr[i].pass[1] = -1;
 		}
 	}
 	return 0;
